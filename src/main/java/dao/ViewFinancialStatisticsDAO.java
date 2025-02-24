55e0e90002d328dd7a82c11fa2625757d5b455be
NguyenNghj <nghintce180897@fpt.edu.vn>
2025-02-24 10:23:43 +0700

    fix dbcontext va viewfinancialstatistic

--- src/main/java/dao/ViewFinancialStatisticsDAO.java
+++ src/main/java/dao/ViewFinancialStatisticsDAO.java
@@ -15,15 +15,15 @@ import java.sql.ResultSet;
 public class ViewFinancialStatisticsDAO extends DBContext {
 
     public double getTotaMoneyByMonth(int month) {
-        String sql = "SELECT SUM(total_amount) AS totalprice  \n" +
-"FROM [pawfect].[dbo].[Orders]  \n" +
-"WHERE MONTH(order_date) = 2 AND status = N'Hoàn thành';";
+        String sql = "SELECT SUM(total_amount) AS totalprice  \n"
+                + "FROM [pawfect].[dbo].[Orders]  \n"
+                + "WHERE MONTH(order_date) = ? AND status = N'Hoàn thành';";
         try {
             PreparedStatement st = conn.prepareStatement(sql);
             st.setInt(1, month);
             ResultSet rs = st.executeQuery();
-            while(rs.next()){
-            return rs.getDouble(1);
+            while (rs.next()) {
+                return rs.getDouble(1);
             }
         } catch (Exception e) {
         }
