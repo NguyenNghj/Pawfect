28086fe70045ae66a724ee5e0d5ac6b98b76b467
LENOVO <LENOVO@LAPTOP-GC911T05>
2025-03-04 22:48:58 +0700

    gắn fillter cho dashboard

---
+++ src/main/java/Fillter/DashboardFillter.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
+ */
+
+package Fillter;
+
+import java.io.IOException;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class DashboardFillter implements Filter {
+
+     @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        boolean hasStaffId = false;
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("staffId".equals(cookie.getName())) {
+                    hasStaffId = true;
+                    break;
+                }
+            }
+        }
+        
+        if (!hasStaffId) {
+            res.sendRedirect(req.getContextPath() + "/loginstaff"); 
+            return;
+        }
+        
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {}
+}
