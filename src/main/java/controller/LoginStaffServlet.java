9492d46613ffdea982572e0bf2665f9c225712d8
Quang Duc <vuquangduc1404@gmail.com>
2025-03-12 10:51:22 +0700

    Fix them gio hang, switch tu admin qua staff con loi~

--- src/main/java/controller/LoginStaffServlet.java
+++ src/main/java/controller/LoginStaffServlet.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dao.UserDAO;
@@ -24,34 +23,37 @@ import model.AccountStaff;
  * @author LENOVO
  */
 public class LoginStaffServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
+        try ( PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet LoginStaffServlet</title>");  
+            out.println("<title>Servlet LoginStaffServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet LoginStaffServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet LoginStaffServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -59,13 +61,14 @@ public class LoginStaffServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher("loginadmin.jsp");
         dispatcher.forward(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -73,44 +76,47 @@ public class LoginStaffServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         String email = request.getParameter("email");
         String password = request.getParameter("password");
-   try {
+        try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] hashBytes = md.digest(password.getBytes());
             StringBuilder sb = new StringBuilder();
             for (byte b : hashBytes) {
-                sb.append(String.format("%02x", b)); 
+                sb.append(String.format("%02x", b));
             }
             String hashedPassword = sb.toString();
-        UserDAO userDAO = new UserDAO();
-        AccountStaff account = new AccountStaff();
-        account = userDAO.loginStaff(email,hashedPassword);
-        if (!AccountStaff.IsEmpty(account)) {
-            Cookie staffId = new Cookie("staffId", account.getStaffId());
-            staffId.setMaxAge(60 * 60 * 24 * 1);
-            response.addCookie(staffId);
-            Cookie staffRole = new Cookie("staffRole", account.getRole());
-            staffRole.setMaxAge(60 * 60 * 24 * 1);
-            response.addCookie(staffRole);
-            Cookie staffName = new Cookie("staffName", account.getUsername());
-            staffName.setMaxAge(60 * 60 * 24 * 1);
-            response.addCookie(staffName);
-            if (account.getRole().equals("Admin")){
-            response.sendRedirect("dashboard/admin/dashboard.jsp");
-            }else{    response.sendRedirect("dashboard/staff/dashboard.jsp");}
-        } else {
-            response.sendRedirect("loginadmin.jsp?error=Invalid Credentials");
-        }
-    }catch (NoSuchAlgorithmException e) {
+            UserDAO userDAO = new UserDAO();
+            AccountStaff account = new AccountStaff();
+            account = userDAO.loginStaff(email, hashedPassword);
+            if (!AccountStaff.IsEmpty(account)) {
+                Cookie staffId = new Cookie("staffId", account.getStaffId());
+                staffId.setMaxAge(60 * 60 * 24 * 1);
+                response.addCookie(staffId);
+                Cookie staffRole = new Cookie("staffRole", account.getRole());
+                staffRole.setMaxAge(60 * 60 * 24 * 1);
+                response.addCookie(staffRole);
+                Cookie staffName = new Cookie("staffName", account.getUsername());
+                staffName.setMaxAge(60 * 60 * 24 * 1);
+                response.addCookie(staffName);
+                if (account.getRole().equals("Admin")) {
+                    response.sendRedirect("dashboard/admin/dashboard.jsp");
+                } else {
+                    response.sendRedirect("dashboard/staff/dashboard.jsp");
+                }
+            } else {
+                response.sendRedirect("loginadmin.jsp?error=Invalid Credentials");
+            }
+        } catch (NoSuchAlgorithmException e) {
             throw new ServletException("Lỗi mã hóa mật khẩu MD5", e);
         }
-   
+
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
